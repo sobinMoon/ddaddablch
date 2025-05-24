d8ae8bad42be4855549f9b347b875c323ba08837
윤서빈 <seobeen624@sookmyung.ac.kr>
2025-05-24 18:35:17 +0900

    [feat] 레포지토리 추가 완료

---
+++ src/main/java/com/donation/ddb/Repository/OrganizationUserRepository.java
@@ -0,0 +1,13 @@
+package com.donation.ddb.Repository;
+
+import com.donation.ddb.Domain.OrganizationUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface OrganizationUserRepository extends JpaRepository<OrganizationUser, Long> {
+    Optional<OrganizationUser> findByoId(Long aLong);
+    Optional<OrganizationUser> findByoEmail(String email);
+    Optional<OrganizationUser> findByoWalletAddress(String address);
+    Boolean existsByoEmail(String email);
+}
