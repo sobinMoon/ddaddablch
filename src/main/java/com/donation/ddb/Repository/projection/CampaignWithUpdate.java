0ec3f24f336256bf98c7a4a239ead4a11516d399
sobinMoon <sobin021206@naver.com>
2025-06-05 14:21:44 +0900

    [fix] 홈 화면 최근 소식 API의 응답 형식 수정
    소식 content를 일정 길이로 잘라서 응답하도록 수정

--- src/main/java/com/donation/ddb/Repository/projection/CampaignWithUpdate.java
+++ src/main/java/com/donation/ddb/Repository/projection/CampaignWithUpdate.java
@@ -2,11 +2,13 @@ package com.donation.ddb.Repository.projection;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @Getter
+@Setter
 public class CampaignWithUpdate {
     private Long campaignId;
     private String title;
