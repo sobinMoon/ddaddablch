4b41472aee6f1aac49f2bc30be8619b8628e1188
sobinMoon <sobin021206@naver.com>
2025-06-06 21:19:55 +0900

    [feat] 단체 회원가입 프로필 이미지 등록 구현

--- src/main/java/com/donation/ddb/Controller/OrgUserController.java
+++ src/main/java/com/donation/ddb/Controller/OrgUserController.java
@@ -1,10 +1,12 @@
 package com.donation.ddb.Controller;
 
 
+import com.donation.ddb.Converter.OrganizationUserConverter;
 import com.donation.ddb.Domain.Exception.DataNotFoundException;
 import com.donation.ddb.Domain.Exception.EmailAlreadyExistsException;
 import com.donation.ddb.Dto.Request.OrgEmailVerificationRequestDto;
 import com.donation.ddb.Dto.Request.OrgSignUpForm;
+import com.donation.ddb.ImageStore;
 import com.donation.ddb.Repository.OrganizationUserRepository;
 import com.donation.ddb.Service.EmailService.EmailService;
 import com.donation.ddb.Service.TokenService.JwtTokenProvider;
@@ -18,6 +20,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -74,18 +78,18 @@ public class OrgUserController {
     }
 
     @GetMapping("/verify-email")
-    public ResponseEntity<?> verifyEmail(@RequestParam(value="token") String token){
-        boolean verified=emailService.verifyEmail(token);
+    public ResponseEntity<?> verifyEmail(@RequestParam(value = "token") String token) {
+        boolean verified = emailService.verifyEmail(token);
 
-        if(verified){
+        if (verified) {
             return ResponseEntity.status(HttpStatus.OK).body(
-                    Map.of("success",true,
-                            "message","인증에 성공했습니다.")
+                    Map.of("success", true,
+                            "message", "인증에 성공했습니다.")
             );
-        }else{
-            Map<String,Object> errorResponse=new HashMap<>();
-            errorResponse.put("success",false);
-            errorResponse.put("message","인증에 실패했습니다.");
+        } else {
+            Map<String, Object> errorResponse = new HashMap<>();
+            errorResponse.put("success", false);
+            errorResponse.put("message", "인증에 실패했습니다.");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
                     errorResponse
             );
@@ -93,31 +97,34 @@ public class OrgUserController {
     }
 
     @PostMapping("/sign-up/organization")
-    public ResponseEntity<?> signup(@RequestBody @Valid OrgSignUpForm orgSignUpForm,
-                                    BindingResult bindingResult){
+    public ResponseEntity<?> signup(
+            @RequestPart(name = "request") @Valid OrgSignUpForm orgSignUpForm,
+            BindingResult bindingResult,
+            @RequestPart(name = "image") MultipartFile profileImage
+    ) {
 
         //로깅
         log.info("단체 회원가입 요청 수신: ", orgSignUpForm.getEmail());
 
         //유효성 검증 실패했을 때 처리
-        if(bindingResult.hasErrors()){
-            Map<String, String> errorMap=new HashMap<>();
-            bindingResult.getFieldErrors().forEach(error->{
-                String fieldName=error.getField();
-                String errorMessage=error.getDefaultMessage();
-                errorMap.put(fieldName,errorMessage);
-                log.warn("회원가입 유효성 검증 실패 : {} - {}",fieldName, errorMessage
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+            bindingResult.getFieldErrors().forEach(error -> {
+                String fieldName = error.getField();
+                String errorMessage = error.getDefaultMessage();
+                errorMap.put(fieldName, errorMessage);
+                log.warn("회원가입 유효성 검증 실패 : {} - {}", fieldName, errorMessage
                 );
             });
             return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
                     .body(
-                            Map.of("success","false",
-                                    "message","회원가입 유효성 검증 싈패 ")
+                            Map.of("success", "false",
+                                    "message", "회원가입 유효성 검증 싈패 ")
                     );
         }
 
         try {
-            Long userId=orgUserService.createOrg(
+            Long userId = orgUserService.createOrg(
                     orgSignUpForm.getName(),
                     orgSignUpForm.getEmail(),
                     orgSignUpForm.getPassword(),
@@ -127,50 +134,37 @@ public class OrgUserController {
                     orgSignUpForm.getProfileImage()
             );
 
-            log.info("회원가입 성공 : 사용자 ID {} ",userId);
+            log.info("image upload : {}", profileImage);
 
+            if (profileImage != null) {
+                log.info("image upload : {}", profileImage.getOriginalFilename());
+                String imagePath = ImageStore.storeImage(profileImage, "\\organization\\" + userId + "\\");
+                orgSignUpForm.setProfileImage(imagePath);
+                orgUserService.updateOrgUser(orgSignUpForm, userId);
+            }
 
-//            //  권한 설정 (기본적으로 ROLE_USER 또는 ROLE_STUDENT 등)
-//            List<GrantedAuthority> authorities = List.of(
-//                    new SimpleGrantedAuthority(ROLE_STUDENT.name()));
-//
-//
-//            //Authentication 객체 생성
-//            Authentication auth=new UsernamePasswordAuthenticationToken(
-//                    new org.springframework.security.core.userdetails.User(
-//                            studentSignUpForm.getSEmail(), "", authorities
-//                    ),
-//                    null,
-//                    authorities
-//            );
-//
-//
-//            //JWT 토큰 생성
-//            String jwt=jwtTokenProvider.generateToken(auth);
-            //log.info("==> 저장 직전 oWalletAddress: " + organizationUserRepository.findByoWalletAddress());
+            log.info("회원가입 성공 : 사용자 ID {} ", userId);
+
             return ResponseEntity.status(HttpStatus.CREATED).body(
-                    Map.of("success",true,
-                            "message","단체 회원가입이 완료됐습니다.")
+                    Map.of("success", true,
+                            "message", "단체 회원가입이 완료됐습니다.")
             );
-        }catch(IllegalArgumentException | IllegalStateException e){
-            log.error("회원가입 처리 중 오류 발생 ",e.getMessage());
+        } catch (IllegalArgumentException | IllegalStateException e) {
+            log.error("회원가입 처리 중 오류 발생 ", e.getMessage());
             return ResponseEntity.badRequest().body(e.getMessage());
-        }catch(EmailAlreadyExistsException e){
-            log.error("이미 회원 가입 되어있는 경우 오류 발생 : {}",e.getMessage());
+        } catch (EmailAlreadyExistsException e) {
+            log.error("이미 회원 가입 되어있는 경우 오류 발생 : {}", e.getMessage());
             return ResponseEntity.status(HttpStatus.CONFLICT)
                     .body(
-                            Map.of("success",false,
-                                    "message",e.getMessage())
-
-
+                            Map.of("success", false,
+                                    "message", e.getMessage())
                     );
-        }
-        catch(DataNotFoundException e){
-            log.error("회원가입 처리 중 데이터 관련 오류 : {}",e.getMessage());
+        } catch (DataNotFoundException e) {
+            log.error("회원가입 처리 중 데이터 관련 오류 : {}", e.getMessage());
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body(e.getMessage());
-        }catch(Exception e){
-            log.error("회원가입 처리 중 오류 : {}",e.getMessage(),e);
+        } catch (Exception e) {
+            log.error("회원가입 처리 중 오류 : {}", e.getMessage(), e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("서버 오류 발생");
         }
